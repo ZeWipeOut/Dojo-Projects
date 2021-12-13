5574aace5132545edae7dff257d904cd3f280956
Arthur Carter <CarterBArthur@gmail.com>
2021-12-13 14:08:47 -0800

    Indentation

---
+++ WebFundamentals12-13/IndentPart1.html/HTMLparents.ts
@@ -0,0 +1,6 @@
+HTML is parent to:  Head, Body, h1, li.
+Head is Parent to: Title.
+Body is Parent to: Table.
+     H1 is parent to: ul.
+Table is parent to: Thea & tbody.
+Tbody is parent to: tr & td
